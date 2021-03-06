1fb4a8ffd1b4b41002c73ceb0e1339783e741451
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-06 23:12:45 +0200

    render TweetForm in view

--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
 
+import TweetForm from '../components/Form';
 import TweetsList from '../components/TweetsList';
 
 const HomeView: FC = () => {
 
   return (
-    <TweetsList />
+    <>
+      <TweetForm />
+      <TweetsList />
+    </>
   );
 }
 
