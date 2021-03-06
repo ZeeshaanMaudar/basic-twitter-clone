fd2d2e1b7f2f28cde15347bd7c09ca9f09bc63ab
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-06 21:45:02 +0200

    create selectors for usersDetails

--- src/redux/users/usersSelectors.ts
+++ src/redux/users/usersSelectors.ts
@@ -1,15 +1,33 @@
 import { createSelector } from 'reselect';
 
+interface User {
+  id: number,
+  username: string,
+  role: string,
+  usersDetailsId: number,
+  profilePic: string
+}
+
+interface UserDetails {
+  id: number,
+  firstName: string,
+  lastName: string,
+  birthday: string
+}
 interface State {
   users: {
-    isFetchingUsers: false,
-    usersList: [],
-    error: null
+    isFetchingUsers: boolean,
+    usersList: User[],
+    errorUsers: string | null,
+    isFetchingUsersDetails: boolean,
+    usersDetailsList: UserDetails[]
+    errorUsersDetails: string | null
   }
 }
 
 const selectUsers = (state: State) => state.users;
 
+// users
 export const selectIsFetchingUsers = createSelector(
   [selectUsers],
   users => users.isFetchingUsers
@@ -22,5 +40,21 @@ export const selectUsersList = createSelector(
 
 export const selectErrorFetchingUsers = createSelector(
   [selectUsers],
-  users => users.error
+  users => users.errorUsers
+);
+
+// users details 
+export const selectIsFetchingUsersDetails = createSelector(
+  [selectUsers],
+  users => users.isFetchingUsersDetails
+);
+
+export const selectUsersDetailsList = createSelector(
+  [selectUsers],
+  users => users.usersDetailsList
+);
+
+export const selectErrorFetchingUsersDetails = createSelector(
+  [selectUsers],
+  users => users.errorUsersDetails
 );
