a817ab483e39ec8a4c5adccbc1ea5e10e671494b
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-06 20:20:07 +0200

    create temporary layout for tweet

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,26 +1,40 @@
 import React, { FC } from 'react';
 
-interface TweetProps {
+interface TweetCardProps {
   tweetItem: {
     id: number,
     tweet: string,
     date: string,
     claps: number,
     userId: number
-  }
+  },
+  user: {
+    id: number,
+    username: string,
+    role: string,
+    usersDetailsId: number,
+    profilePic: string
+  } | undefined
 }
 
-const Tweet: FC<TweetProps> = ({ tweetItem }) => {
+const Tweet: FC<TweetCardProps> = ({ tweetItem, user }) => {
 
-  console.log('tweetItem: ', tweetItem);
-
-  const { id, tweet, date, claps, userId } = tweetItem;
+  const { tweet, date, claps, userId } = tweetItem;
+  const { username, role, usersDetailsId, profilePic } = user;
 
   return (
     <div style={{ border: '1px solid red'}}>
-      <div>{date}</div>
-      <div>{tweet}</div>
-      <div>{claps}</div>
+      <div>
+        <img src={profilePic} alt={`${username}'s profile avatar`} style={{ width: '50px', height: '50px'}} />
+      </div>
+      <div>
+        <div>
+          <span>{username}</span>
+          <span>{date}</span>
+        </div>
+        <p>{tweet}</p>
+        <div>Claps: {claps}</div>
+      </div>
     </div>
   );
 }
