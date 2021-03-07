22d27e0de5681ec60a3407a6211f534413ab0ca0
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-07 22:56:14 +0200

    add logic to render image

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,19 +1,52 @@
 import React, { FC, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { postTweetStartAsync } from '../../redux/tweets/tweetsActions';
+import { selectUsersList, selectIsFetchingUsers } from '../../redux/users/usersSelectors';
 
 import {
   Wrapper,
   TextArea,
-  Button
+  Button,
+  ProfilePic,
+  FormHeader,
+  FormFooter
 } from './styled';
+
+interface User {
+  id: number,
+  username: string,
+  role: string,
+  usersDetailsId: number,
+  profilePic: string
+}
+
+const callImage = (usersList: User[], isLoadingList: boolean) => {
+
+  if (isLoadingList) {
+    return <div>Loading...</div>
+  }
+
+  if (usersList.length > 0) {
+    const userInfoList = usersList.filter(user => user.id === 1);
+    const image = userInfoList[0].profilePic;
+
+    return (
+      <ProfilePic src={image} alt='profile avatar' />
+    );
+  }
+
+}
 
 const TweetForm: FC = () => {
 
   const [newTweet, setNewTweet] = useState('');
 
   const dispatch = useDispatch();
+
+
+  const isLoadingList = useSelector(selectIsFetchingUsers);
+  const usersList = useSelector(selectUsersList);
 
   const handleChange = (event: { target: { value: React.SetStateAction<string>; }; }) => {
     setNewTweet(event.target.value);
@@ -30,14 +63,19 @@ const TweetForm: FC = () => {
   return (
     <Wrapper>
       <form onSubmit={handleSubmit}>
-        <TextArea
-          name='newTweet'
-          value={newTweet}
-          onChange={handleChange}
-          placeholder="What's happening?"
-          rows={3}
-        />
-        <Button type='submit' disabled={newTweet.length <= 0}>Tweet</Button>
+        <FormHeader>
+          {callImage(usersList, isLoadingList)}
+          <TextArea
+            name='newTweet'
+            value={newTweet}
+            onChange={handleChange}
+            placeholder="What's happening?"
+            rows={3}
+          />
+        </FormHeader>  
+        <FormFooter>
+          <Button type='submit' disabled={newTweet.length <= 0}>Tweet</Button>
+        </FormFooter>
       </form>
     </Wrapper>
   );
