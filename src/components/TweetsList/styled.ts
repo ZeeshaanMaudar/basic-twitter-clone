f6e4d234d583c7b16cdc266937308b7c40215d27
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-07 21:13:02 +0200

    add styling to tweetlist

---
+++ src/components/TweetsList/styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+
+export const Wrapper = styled.div`
+  border: 1px solid #eee;
+  padding: 10px;
+`;
+
+export const Title = styled.h2`
+  color: #141F45;
+  font-family: Roboto;
+  font-weight: 700;
+  font-size: 25px;
+  line-height: 42px;
+  letter-spacing: 2px;
+  margin: 0;
+  margin-bottom: 30px;
+`;
+
