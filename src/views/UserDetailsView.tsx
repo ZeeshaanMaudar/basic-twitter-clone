12aaced5d226250667e46381f3aba03119c74e4f
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-07 18:18:00 +0200

    render TweetList and Pagination components

--- src/views/UserDetailsView.tsx
+++ src/views/UserDetailsView.tsx
@@ -1,14 +1,20 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 import { fetchSingleUserStartAsync, fetchSingleUserDetailsStartAsync } from '../redux/singleUser/singleUserActions';
 
 import Profile from '../components/Profile';
+import TweetsList from '../components/TweetsList';
+import Pagination from '../components/Pagination';
+
 
 const UserDetailsView: FC = () => {
 
   const { userId } = useParams<{ userId: string }>();
+
+  const [limit, setLimit] = useState(5);
+  const [page, setPage] = useState(1);
 
   const dispatch = useDispatch();
 
@@ -24,6 +30,8 @@ const UserDetailsView: FC = () => {
   return (
     <div>
       <Profile />
+      <TweetsList {...{ page, limit }} />
+      <Pagination {...{ page, limit, setLimit, setPage }} />
       <div>Tweets Section</div>
       <div>Statistics Section</div>
     </div>
