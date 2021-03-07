a52f9427e1d6cce4478131156c914a184109f8ce
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-07 16:01:39 +0200

    create actions for userDetails

--- src/redux/singleUser/singleUserActions.ts
+++ src/redux/singleUser/singleUserActions.ts
@@ -10,14 +10,21 @@ interface User {
   profilePic: string
 }
 
+interface UserDetails {
+  id: number,
+  firstName: string,
+  lastName: string,
+  birthday: string
+}
+
 // fetch users list
 export const fetchSingleUserRequest = () => ({
   type: actionTypes.FETCH_SINGLE_USER_REQUEST,
 });
 
-export const fetchSingleUserSuccess = (usersList: User[]) => ({
+export const fetchSingleUserSuccess = (user: User) => ({
   type: actionTypes.FETCH_SINGLE_USER_SUCCESS,
-  payload: usersList
+  payload: user
 });
 
 export const fetchSingleUserFailure = (error: string) => ({
@@ -39,6 +46,39 @@ export const fetchSingleUserStartAsync = (userId: number) => {
       })
       .catch(error => {
         dispatch(fetchSingleUserFailure(error));
+      })
+  }
+};
+
+// fetch single user details list
+export const fetchSingleUserDetailsRequest = () => ({
+  type: actionTypes.FETCH_SINGLE_USER_DETAILS_REQUEST,
+});
+
+export const fetchSingleUserDetailsSuccess = (userDetails: UserDetails) => ({
+  type: actionTypes.FETCH_SINGLE_USER_DETAILS_SUCCESS,
+  payload: userDetails
+});
+
+export const fetchSingleUserDetailsFailure = (error: string) => ({
+  type: actionTypes.FETCH_SINGLE_USER_DETAILS_FAILURE,
+  payload: error
+});
+
+export const fetchSingleUserDetailsStartAsync = (userDetailsId: number) => {
+
+  return (dispatch: any) => {
+
+    dispatch(fetchSingleUserDetailsRequest());
+
+    const endpoint = `${process.env.REACT_APP_API}/usersDetails/${userDetailsId}`;
+
+    axios.get(endpoint)
+      .then(response => {
+        dispatch(fetchSingleUserDetailsSuccess(response.data));
+      })
+      .catch(error => {
+        dispatch(fetchSingleUserDetailsFailure(error));
       })
   }
 };
