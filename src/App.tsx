cfc84e2cbc712f1845f5d3e0375af823de8c0aaa
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-07 14:48:30 +0200

    create route for user details page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import HomeView from './views/HomeView';
+import UserDetailsView from './views/UserDetailsView';
 
 const App = () => {
   return (
     <Switch>
       <Route exact path='/' component={HomeView} />
+      <Route exact path='/:userId' component={UserDetailsView} />
       <Redirect to='/' />
     </Switch>
   );
