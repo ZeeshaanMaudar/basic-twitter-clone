390adc61bb3863a48f23e2e52cfa9506becdecf6
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-07 16:01:54 +0200

    create reducers for userDetails

--- src/redux/singleUser/singleUserReducer.ts
+++ src/redux/singleUser/singleUserReducer.ts
@@ -4,6 +4,9 @@ const initialState = {
     isFetchingUser: false,
     user: null,
     errorUser: '',
+    isFetchingSingleUserDetails: false,
+    userDetails: null,
+    errorUserDetails: ''
 };
 
 const singleUserReducer = (state = initialState, action: any) => {
@@ -17,7 +20,7 @@ const singleUserReducer = (state = initialState, action: any) => {
         case actionTypes.FETCH_SINGLE_USER_SUCCESS:
             return {
                 ...state,
-                usersList: action.payload,
+                user: action.payload,
                 isFetchingUsers: false
             }
         case actionTypes.FETCH_SINGLE_USER_FAILURE:
@@ -27,6 +30,24 @@ const singleUserReducer = (state = initialState, action: any) => {
                 isFetchingUsers: false
             }
     
+        // fetch single user details list
+        case actionTypes.FETCH_SINGLE_USER_DETAILS_REQUEST:
+            return {
+                ...state,
+                isFetchingSingleUserDetails: true
+            }
+        case actionTypes.FETCH_SINGLE_USER_DETAILS_SUCCESS:
+            return {
+                ...state,
+                userDetails: action.payload,
+                isFetchingSingleUserDetails: false
+            }
+        case actionTypes.FETCH_SINGLE_USER_DETAILS_FAILURE:
+            return {
+                ...state,
+                errorUserDetails: action.payload,
+                isFetchingSingleUserDetails: false
+            }
         default:
             return state;
     }
