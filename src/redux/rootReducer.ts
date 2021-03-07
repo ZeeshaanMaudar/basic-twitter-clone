95bcdbd93624854dae0a74d62fe31ed3072cf59b
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2021-03-07 15:27:08 +0200

    combine single user to root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import tweetsReducer from './tweets/tweetsReducer';
 import usersReducer from './users/usersReducer';
+import singleUserReducer from './singleUser/singleUserReducer';
 
 const rootReducer = combineReducers({
   tweets: tweetsReducer,
-  users: usersReducer
+  users: usersReducer,
+  singleUser: singleUserReducer
 });
 
 export default rootReducer;
